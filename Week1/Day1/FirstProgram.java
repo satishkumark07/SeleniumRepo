b809a8d42a8ad0311099e7db52964257c3487dd9
satishkumark07 <satishkumark07@gmail.com>
2024-08-12 22:51:05 +0530

    Add files via upload
    week 1 - day 1 assigments

---
+++ Week1/Day1/FirstProgram.java
@@ -0,0 +1,12 @@
+package Week1.Day1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is first program");
+		System.out.println("this is secondline");
+	}
+
+}
---
+++ Week1/Day1/Variables.java
@@ -0,0 +1,20 @@
+package Week1.Day1;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float version=130.21f;
+		String browser="firefox";
+		boolean isVisible=true;
+		int releaseYear=1998;
+		char browserLogo='c';
+		
+		System.out.println("Browser version:"+version);
+		System.out.println("Browser name:"+browser);
+		System.out.println("browser is visible"+isVisible+"\n"+"release year"+releaseYear+"\n"+"Browser logo"+browserLogo);
+		
+	}
+
+}
